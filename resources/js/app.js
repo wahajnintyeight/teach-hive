77bac693d74431694f4751d430a8e59884ce9f96
wahajnintyeight <wahajdorift@yahoo.com>
2023-01-22 20:12:48 +0500

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+import { createApp } from 'vue'
+// import Test from './ /components/Test'
+import Test from './components/Test.vue'
+
+console.log("HI")
+createApp(Test).mount("#app")
---
+++ resources/js/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{ title }}</h1>
+    <form action="" method="post">
+        <h2>Hiiiiiiiiiiiiiiii</h2>
+        <input type="datetime" name="date" id="">
+        <input type="checkbox" name="" id="">
+    </form>
+</template>
+<script>
+export default {
+    setup: () => ({
+        title: 'How To Install Vue 3 In Laravel 9 Step By Step'
+    })
+}
+</script>
